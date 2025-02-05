37f9fbb160f8159a81571555c1e42baef5ca08ce
stalindlcruz <stalindlcruz@gmail.com>
2025-02-05 14:01:50 -0500

    first commit -> adding main.ts

---
+++ main.ts
@@ -0,0 +1,8 @@
+/* Hacer un sistema de compra utilizando programacion Orientada a Objetos (POO), que el sistema de compra tenga un inventario de productos, un inventario de usuario, que tenga acceso a un carrito de compra donde se puedan ir agregando productos y esos productos deben tener su caracteristicas, su descripcion, precio, cantidad, etc. Al final debe de tener una opcion de pagar que imprima por mensaje la cantidad que esa persona debe de pagar dependiendo de los productos que este agrego al carrito, que le diga al usuario debes de pagar tal cantidad y cuando el usuario le de a confirmar se le imprima una factura con toda la descripcion (la fecha, el total, los productos, nombre del cliente, nombre del empleado, etc).
+
+OJO:
+La opcion que diga pagar y confirmar son metodos, porque no habra nada visual.
+
+Con esta practica se va a practicar todo lo que hemos visto, getter, setter, los pilares de la POO, etc. Se empezara a utilizar el concepto CRUD (create, read, update, delete), esto se implementara porque se agregara un usuario, eliminara, actualizarlo y buscarlo o filtrarlo.
+
+Recomendacion: empezar por lo mas basico que es modelando cada clase de lo que necesitare. */
